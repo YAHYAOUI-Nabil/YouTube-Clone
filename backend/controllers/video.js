1dd43a2c35ecd358953bfb59fd5cbc3b0251b453
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-09-20 12:17:13 +0200

    fix dotenv file error

--- backend/controllers/video.js
+++ backend/controllers/video.js
@@ -1,7 +1,14 @@
 const Video = require('../models/video')
 
-exports.addVideo = (req, res) => {
-    res.send('hello')
+exports.addVideo = async (req, res) => {
+    const newVideo = new Product(req.body);
+
+    try {
+        const savedVideo = await newVideo.save();
+        res.status(200).json(savedVideo);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 }
 exports.getVideos = (req, res) => {
     res.send('hello get videos')
